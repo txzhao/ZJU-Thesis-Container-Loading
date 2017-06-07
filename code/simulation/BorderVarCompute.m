d197c0d2ade919114706473c3a9aa43acc9479ec
Tianxiao Zhao <tzh@kth.se>
2017-06-07 11:31:51 +0200

    Update BorderVarCompute.m

--- code/simulation/BorderVarCompute.m
+++ code/simulation/BorderVarCompute.m
@@ -1,8 +1,11 @@
 function BorderVar = BorderVarCompute(Border)
 WBox = max(size(Border,1),size(Border,2));
+
 for i = 1 : WBox - 2
     Border_center(i) = Border(i+1);
 end
+
 flag = 0;
 BorderVar = std(Border_center,flag);
+
 end
