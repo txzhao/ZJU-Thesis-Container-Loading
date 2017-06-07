27aa7793b332bf9ed3794ea3d6f117862c7a682d
Tianxiao Zhao <tzh@kth.se>
2017-06-07 11:31:01 +0200

    Update BoxPackingTest.m

--- code/simulation/BoxPackingTest.m
+++ code/simulation/BoxPackingTest.m
@@ -1,8 +1,9 @@
 %====================BoxPackingTest===================%
-% [BoxAmount] = xlsread('data_empty');
+% read in data
 [BoxAmount] = xlsread('BR1');
 minBorderVar = 20;
+
 [BoxStack,Border,dAreaRatio_best,dSpaceRatio_best,C_best, FinishedBox_best,minBlankPoints,...
-    BorderVars,BoxPackingInfo_record] = BoxPacking3(BoxAmount,minBorderVar);
-% [dAreaRatio_best,C_best, FinishedBox_best,minBlankPoints,BoxPackingInfo_record] = BoxPacking2(BoxAmount);
+    BorderVars,BoxPackingInfo_record] = BoxPacking(BoxAmount,minBorderVar);
+
 imshow(1-C_best,[-2,1],'InitialMagnification','fit');
